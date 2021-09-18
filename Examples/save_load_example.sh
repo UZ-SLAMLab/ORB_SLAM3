d2f6ddd14091b1027d5ae5f933133f1b881a09a6
Thomas Dutrannois <tomtom65@hotmail.be>
2021-09-18 12:14:43 +0200

    Add Mono code lines

--- Examples/save_load_example.sh
+++ Examples/save_load_example.sh
@@ -1,13 +1,23 @@
 #!/bin/bash
 pathDatasetEuroc='/home/thomas/EuRoC' #Example, it is necesary to change it by the dataset path
 
+# Monocular Save Example
+#------------------------------------
+echo "Launching MH01 with Monocular sensor for Map Saving"
+./Monocular/mono_save ../Vocabulary/ORBvoc.txt ./Monocular/EuRoC.yaml ${pathDatasetEuroc}/MH01 ./Monocular/EuRoC_TimeStamps/MH01.txt MH01_mono_save
+
+# Monocular Load Example
+#------------------------------------
+echo "Launching MH01 with Monocular sensor for Map Loading"
+./Monocular/mono_load ../Vocabulary/ORBvoc.txt ./Monocular/EuRoC.yaml ${pathDatasetEuroc}/MH01 ./Monocular/EuRoC_TimeStamps/MH01.txt MH01_mono_save
+
+'
 #------------------------------------
 # Stereo Save Example
 echo "Launching MH01 with Stereo sensor for Map Saving"
-./Stereo/stereo_save ../Vocabulary/ORBvoc.txt ./Stereo/EuRoC.yaml /home/thomas/EuRoC/MH01 ./Stereo/EuRoC_TimeStamps/MH01.txt MH01_save
+./Stereo/stereo_save ../Vocabulary/ORBvoc.txt ./Stereo/EuRoC.yaml ${pathDatasetEuroc}/MH01 ./Stereo/EuRoC_TimeStamps/MH01.txt MH01_stereo_save
 
 #------------------------------------
 # Stereo Load Example
 echo "Launching MH01 with Stereo sensor for Map Loading"
-./Stereo/stereo_load ../Vocabulary/ORBvoc.txt ./Stereo/EuRoC.yaml /home/thomas/EuRoC/MH01 ./Stereo/EuRoC_TimeStamps/MH01.txt MH01_save
-
+./Stereo/stereo_load ../Vocabulary/ORBvoc.txt ./Stereo/EuRoC.yaml ${pathDatasetEuroc}/MH01 ./Stereo/EuRoC_TimeStamps/MH01.txt MH01_stereo_save
