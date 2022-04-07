e80340a7dbc8f19639d176cfdbd2f1ae0496a4fd
文旅小七 <oncwnuARU8Q7HxicmNMBmvA284cU@git.weixin.qq.com>
2022-04-07 15:29:50 +0800

    test example & bin vocab

---
+++ Examples/Monocular/myVideo.cpp
@@ -0,0 +1,44 @@
+#include <opencv2/opencv.hpp>
+
+#include "System.h"
+
+#include <string>
+#include <chrono>   // for time stamp
+#include <iostream>
+
+using namespace std;
+
+string parameterFile = "./HuaWeiMatePro30.yaml";
+string vocFile = "/home/lighthouse/orb-slam3/ORB_SLAM3/Vocabulary/ORBvoc.bin";
+
+string videoFile = "/home/lighthouse/orb-slam3/testVideo/test.mp4";
+
+int main(int argc, char **argv) {
+
+// 声明 ORB-SLAM2 系统
+ORB_SLAM3::System SLAM(vocFile, parameterFile, ORB_SLAM3::System::MONOCULAR, true);
+// 获取视频图像
+cv::VideoCapture cap(videoFile);    // change to 1 if you want to use USB camera.
+// 记录系统时间
+auto start = chrono::system_clock::now();
+
+while (1) {
+        cv::Mat frame;
+        cap >> frame;   // 读取相机数据
+        if ( frame.data == nullptr )
+            break;
+
+        // rescale because image is too large
+        cv::Mat frame_resized;
+        cv::resize(frame, frame_resized, cv::Size(640,480));
+
+        auto now = chrono::system_clock::now();
+        auto timestamp = chrono::duration_cast<chrono::milliseconds>(now - start);
+        SLAM.TrackMonocular(frame_resized, double(timestamp.count())/1000.0);
+        cv::waitKey(30);
+    }
+
+    SLAM.Shutdown();
+    return 0;
+
+}
