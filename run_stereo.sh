2842ef454f8aa89510908ac4de02968ca305d6b9
olayasturias <olaya_93@hotmail.es>
2022-09-23 16:14:32 +0200

    bash files for easy running of algorithms

--- run_stereo.sh
+++ run_stereo.sh
@@ -2,4 +2,4 @@
 echo "Environment: $1";
 echo "Track: $2";
 
-./Examples/Stereo/stereo_mimir /home/olaya/dev/ORB_SLAM3/Vocabulary/ORBvoc.txt Examples/Stereo/MIMIR.yaml $HOME/Datasets/MIMIR/$1/$2 $HOME/Datasets/MIMIR/$1/$2/auv0/ORB_timestamps.txt /home/olaya/Datasets/MIMIR/$1/$2 /home/olaya/Datasets/MIMIR/$1/$2/auv0/ORB_timestamps.txt $1_$2_cam2.txt
+./Examples/Stereo/stereo_mimir /home/olaya/dev/ORB_SLAM3/Vocabulary/ORBvoc.txt Examples/Stereo/MIMIR.yaml $HOME/Datasets/MIMIR/$1/$2 Examples/Monocular/MIMIR_TimeStamps/$1_$2.txt /home/olaya/Datasets/MIMIR/$1/$2 Examples/Monocular/MIMIR_TimeStamps/$1_$2.txt $1_$2_cam2.txt
