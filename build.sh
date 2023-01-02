deac0e09c8e75263b5ff8196cf36d85139831a8a
ben <ben@seetaandben.com>
2023-01-02 20:07:36 +0000

    make CMakeLists behave appropriately on intel or M1.  Update build.sh to do the same

--- build.sh
+++ build.sh
@@ -1,10 +1,16 @@
 set -e
 echo "Configuring and building Thirdparty/DBoW2 ..."
 
+# note that uname -m returns intel when running on a M1 mac if it's running under Rosetta
+# this is very annoying
+EXTRA_CMAKE_ARGS="-DCMAKE_APPLE_SILICON_PROCESSOR=`uname -m`"
+
+echo Running with extra cmake args: ${EXTRA_CMAKE_ARGS}
+
 cd Thirdparty/DBoW2
 mkdir -p build
 cd build
-cmake .. -DCMAKE_BUILD_TYPE=Release -DCMAKE_APPLE_SILICON_PROCESSOR=arm64
+cmake .. -DCMAKE_BUILD_TYPE=Release ${EXTRA_CMAKE_ARGS}
 make -j
 
 cd ../../g2o
@@ -12,7 +18,7 @@ cd ../../g2o
 echo "Configuring and building Thirdparty/g2o ..."
 mkdir -p build
 cd build
-cmake .. -DCMAKE_BUILD_TYPE=Release -DCMAKE_BUILD_TYPE=Release -DCMAKE_APPLE_SILICON_PROCESSOR=arm64
+cmake .. -DCMAKE_BUILD_TYPE=Release -DCMAKE_BUILD_TYPE=Release ${EXTRA_CMAKE_ARGS}
 make -j
 
 cd ../../Sophus
@@ -21,7 +27,7 @@ echo "Configuring and building Thirdparty/Sophus ..."
 
 mkdir -p build
 cd build
-cmake .. -DCMAKE_BUILD_TYPE=Release -DCMAKE_BUILD_TYPE=Release -DCMAKE_APPLE_SILICON_PROCESSOR=arm64
+cmake .. -DCMAKE_BUILD_TYPE=Release -DCMAKE_BUILD_TYPE=Release ${EXTRA_CMAKE_ARGS}
 make -j
 
 cd ../../../
@@ -36,5 +42,5 @@ echo "Configuring and building ORB_SLAM3 ..."
 
 mkdir -p build
 cd build
-cmake .. -DCMAKE_BUILD_TYPE=Release -DCMAKE_BUILD_TYPE=Release -DCMAKE_APPLE_SILICON_PROCESSOR=arm64
+cmake .. -DCMAKE_BUILD_TYPE=Release -DCMAKE_BUILD_TYPE=Release ${EXTRA_CMAKE_ARGS}
 make -j4
