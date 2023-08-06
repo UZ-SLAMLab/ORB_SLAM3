ebd37ed216bd219376a7fa122fefd4bea0e1a6ff
ron-lbb <ron@libba-xr.com>
2023-08-06 15:48:46 +0300

    verbose cp

--- android_build/build_android.sh
+++ android_build/build_android.sh
@@ -131,33 +131,33 @@ cd $SLAM_ROOT
 echo "Copying Shared Objects"
 JNI_PATH=$APP_PATH/ServiceApp/com.LibbaInc.ltd/app/src/main/jniLibs/$ANDROID_ABI
 mkdir -p $JNI_PATH
-cp -r -f -a Thirdparty/g2o/lib/libg2o.so $JNI_PATH
-cp -r -f -a Thirdparty/openssl/libcrypto.so $JNI_PATH
-cp -r -f -a Thirdparty/openssl/libcrypto.so.1.1 $JNI_PATH
-cp -r -f -a lib/* $JNI_PATH
+cp -r -f -v -a Thirdparty/g2o/lib/libg2o.so $JNI_PATH
+cp -r -f -v -a Thirdparty/openssl/libcrypto.so $JNI_PATH
+cp -r -f -v -a Thirdparty/openssl/libcrypto.so.1.1 $JNI_PATH
+cp -r -f -v -a lib/* $JNI_PATH
 
 echo "Copying Headers"
 APP_HEADERS_PATH=$APP_PATH/ServiceApp/com.LibbaInc.ltd/app/src/main/jni
 SLAM_HEADERS_PATH=$APP_HEADERS_PATH/SLAM
 mkdir -p $SLAM_HEADERS_PATH
-cp -r -f include/* $SLAM_HEADERS_PATH
+cp -r -f -v include/* $SLAM_HEADERS_PATH
 DBOW2_HEADERS_PATH=$SLAM_HEADERS_PATH/Thirdparty/DBoW2
 mkdir -p $DBOW2_HEADERS_PATH
-cp -r -f Thirdparty/DBoW2/DBoW2 Thirdparty/DBoW2/DUtils  $DBOW2_HEADERS_PATH
+cp -r -f -v Thirdparty/DBoW2/DBoW2 Thirdparty/DBoW2/DUtils  $DBOW2_HEADERS_PATH
 G2O_HEADERS_PATH=$SLAM_HEADERS_PATH/Thirdparty/g2o
 mkdir -p $G2O_HEADERS_PATH
-cp -r -f Thirdparty/g2o/g2o Thirdparty/g2o/config.h $G2O_HEADERS_PATH
+cp -r -f -v Thirdparty/g2o/g2o Thirdparty/g2o/config.h $G2O_HEADERS_PATH
 SOPHUS_HEADERS_PATH=$SLAM_HEADERS_PATH/Thirdparty/Sophus
 mkdir -p $SOPHUS_HEADERS_PATH
-cp -r -f Thirdparty/Sophus/sophus $SOPHUS_HEADERS_PATH
+cp -r -f -v Thirdparty/Sophus/sophus $SOPHUS_HEADERS_PATH
 EIGEN_HEADERS_PATH=$APP_HEADERS_PATH
 mkdir -p $EIGEN_HEADERS_PATH
-cp -r -f Thirdparty/eigen-$EIGEN_VERSION/Eigen $EIGEN_HEADERS_PATH
+cp -r -f -v Thirdparty/eigen-$EIGEN_VERSION/Eigen $EIGEN_HEADERS_PATH
 BOOST_HEADERS_PATH=$APP_HEADERS_PATH
 mkdir -p $BOOST_HEADERS_PATH
-cp -r -f Thirdparty/Boost-for-Android/boost_1_82_0/boost $BOOST_HEADERS_PATH
+cp -r -f -v Thirdparty/Boost-for-Android/boost_1_82_0/boost $BOOST_HEADERS_PATH
 
 echo "Copying OpenCV"
-cp -r -f Thirdparty/OpenCV-android-sdk $APP_PATH/ServiceApp
+cp -r -f -v Thirdparty/OpenCV-android-sdk $APP_PATH/ServiceApp
 
 echo -e "${GREEN}SUCCESS"
