4ef76146435bd1affed06712f8cd8e68d10fa839
Jaafar <jaafar.a.mahmoud1@gmail.com>
2023-11-02 16:03:42 +0300

    additional cameras

--- src/LocalMapping.cc
+++ src/LocalMapping.cc
@@ -247,7 +247,7 @@ void LocalMapping::Run()
             vdKFCullingSync_ms.push_back(timeKFCulling_ms);
 #endif
 
-            mpLoopCloser->InsertKeyFrame(mpCurrentKeyFrame);
+            //mpLoopCloser->InsertKeyFrame(mpCurrentKeyFrame);
 
 #ifdef REGISTER_TIMES
             std::chrono::steady_clock::time_point time_EndLocalMap = std::chrono::steady_clock::now();
