a31835e59d797613d8d8806d33b9245b2538d7e3
JustMohsen <53262801+JustMohsen@users.noreply.github.com>
2023-11-19 11:32:18 -0800

    change save name

--- Examples_old/Monocular-Inertial/mono_inertial_tum_vi.cc
+++ Examples_old/Monocular-Inertial/mono_inertial_tum_vi.cc
@@ -253,8 +253,8 @@ int main(int argc, char **argv)
 
     if (bFileName)
     {
-        const string kf_file =  "kf_" + string(argv[argc-1]) + ".txt";
-        const string f_file =  "f_" + string(argv[argc-1]) + ".txt";
+        const string kf_file =  string(argv[argc-1]) + "_keyframes.txt";
+        const string f_file =  string(argv[argc-1]) + "frames.txt";
         SLAM.SaveTrajectoryEuRoC(f_file);
         SLAM.SaveKeyFrameTrajectoryEuRoC(kf_file);
     }
