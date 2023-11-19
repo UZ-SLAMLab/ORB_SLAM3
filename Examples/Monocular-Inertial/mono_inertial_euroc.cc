28871767ea117d3698ffe9f209f3ea3f4ad810f4
JustMohsen <53262801+JustMohsen@users.noreply.github.com>
2023-11-18 18:50:21 -0800

    add examples

--- Examples/Monocular-Inertial/mono_inertial_euroc.cc
+++ Examples/Monocular-Inertial/mono_inertial_euroc.cc
@@ -117,7 +117,7 @@ int main(int argc, char *argv[])
     cout.precision(17);
 
     // Create SLAM system. It initializes all system threads and gets ready to process frames.
-    ORB_SLAM3::System SLAM(argv[1],argv[2],ORB_SLAM3::System::IMU_MONOCULAR, true);
+    ORB_SLAM3::System SLAM(argv[1],argv[2],ORB_SLAM3::System::IMU_MONOCULAR, false);
     float imageScale = SLAM.GetImageScale();
 
     double t_resize = 0.f;
--- Examples/Stereo/stereo_euroc.cc
+++ Examples/Stereo/stereo_euroc.cc
@@ -88,7 +88,7 @@ int main(int argc, char **argv)
     cout.precision(17);
 
     // Create SLAM system. It initializes all system threads and gets ready to process frames.
-    ORB_SLAM3::System SLAM(argv[1],argv[2],ORB_SLAM3::System::STEREO, true);
+    ORB_SLAM3::System SLAM(argv[1],argv[2],ORB_SLAM3::System::STEREO, false);
 
     cv::Mat imLeft, imRight;
     for (seq = 0; seq<num_seq; seq++)
