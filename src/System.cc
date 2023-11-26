17e31f26f87d0b7edcb2a6656e93c9e0da29606f
d4n13lx13 <danxie2001@gmail.com>
2023-11-25 22:43:19 -0500

    removed erroneous print statements

--- src/System.cc
+++ src/System.cc
@@ -398,12 +398,9 @@ Sophus::SE3f System::TrackRGBD(const cv::Mat &im, const cv::Mat &depthmap, const
 
 Sophus::SE3f System::TrackMonocular(const cv::Mat &im, const double &timestamp, const vector<IMU::Point>& vImuMeas, string filename)
 {
-    cout << "moose" << endl;
-
     {
         unique_lock<mutex> lock(mMutexReset);
         if(mbShutDown)
-            cout << "Shutdown" << endl;
             return Sophus::SE3f();
     }
 
